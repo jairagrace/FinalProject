3a8330f339d1c11044dfb7cb14610b75ca5670ff
jairaCabarubias <jairagrace16@gmail.com>
2018-03-21 15:22:06 +0800

    Initial Commit

--- app/src/main/java/jaira/cabarrubias/com/finalproject/user/finalproject/MainActivity.kt
+++ app/src/main/java/jaira/cabarrubias/com/finalproject/user/finalproject/MainActivity.kt
@@ -3,15 +3,19 @@ package jaira.cabarrubias.com.finalproject.user.finalproject
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.View.GONE
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var addAlbum = ArrayList<Album>()
+    private var searchAlbum = ArrayList<Album>()
+    private val url = "http://ws.audioscrobbler.com/2.0/?method=album.search&album=believe&api_key=554b0d9dfe8d7959568d15556ecbd485&format=json"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        progressBar.visibility = GONE
 
     }
-}
+    }
